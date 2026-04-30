553618f93d738752cd780d70791b0ef8ee9e1806
Pratul Kumar <pratulkumar2003@gmail.com>
2026-04-30 22:59:32 +0530

    Fix syntax error in dockercompose.groovy

--- vars/dockercompose.groovy
+++ vars/dockercompose.groovy
@@ -1,6 +1,6 @@
 def call(String credentialsId, String variable){
   withCredentials[file(credentialsId: "${credentialsId}", variable: "${variable}")]{
-    sh "sudo rm "${variable}" && sudo cp "${variable}" .
+    sh "sudo rm "${variable}" && sudo cp "${variable}" ."
     sh "docker compose up --build -d"
   }
 }
