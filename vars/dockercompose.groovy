4bbc3c35739c8a5c14cbce962ac79cffd555f250
Pratul Kumar <pratulkumar2003@gmail.com>
2026-04-30 22:48:34 +0530

    Update call function to manage credentials and files
    Refactor call function to use credentials and handle file operations.

--- vars/dockercompose.groovy
+++ vars/dockercompose.groovy
@@ -1,3 +1,6 @@
-def call(){
-  sh "docker compose up --build -d"
+def call(String credentialsId, String variable){
+  withCredentials[file(credentialsId: "${credentialsId}", variable: "${variable}")]{
+    sh "sudo rm "${variable}" && sudo cp "${variable}" .
+    sh "docker compose up --build -d"
+  }
 }
