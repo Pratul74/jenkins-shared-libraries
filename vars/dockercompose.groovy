52abfb602e43bc40d8db1458ad6c109b3a674d04
Pratul Kumar <pratulkumar2003@gmail.com>
2026-05-01 16:19:59 +0530

    Refactor dockercompose.groovy for improved logic
    Update credential handling and add .env file check

--- vars/dockercompose.groovy
+++ vars/dockercompose.groovy
@@ -1,6 +1,10 @@
 def call(String credentialsId, String variable){
-  withCredentials[file(credentialsId: "${credentialsId}", variable: "${variable}")]{
-    sh "sudo rm "${variable}" && sudo cp "${variable}" ."
+  withCredentials([file(credentialsId: credentialsId, variable: variable)]){
+    sh '''
+      if [ ! -f .env ]; then
+        cp $''' + variable + ''' .env
+      fi
+    '''
     sh "docker compose up --build -d"
   }
 }
