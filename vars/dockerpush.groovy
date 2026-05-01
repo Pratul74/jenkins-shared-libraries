efb1205e339d698ee9bcd8abbf0e9c7a0e4fbad1
Pratul Kumar <pratulkumar2003@gmail.com>
2026-05-01 16:36:12 +0530

    Refactor dockerpush function to include username and password variables

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,2 +1,6 @@
-def call(String credentialsId){
-  withCredentials[usernamePassword(credentialsId: "${credentialsId}", 
+def call(String credentialsId, String usernameVariable, String passwordVariable, String imageTag){
+  withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: usernameVariable, passwordvariable: passwordVariable)]){
+    sh "docker login -u ${usernameVariable} -p ${passwordVariable}"
+    sh "docker push usernameVariable/${imageTag}"
+  }
+}
